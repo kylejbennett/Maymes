05a2aa651dc520cbcec5a30f538f96237dcde9a6
Kyle Bennett <kyle.j.bennett5@gmail.com>
2015-12-12 16:42:25 -0600

    added form for creating dishes, added menu display and styling

--- app/controllers/dishes_controller.rb
+++ app/controllers/dishes_controller.rb
@@ -1,11 +1,22 @@
 class DishesController < ApplicationController
   def index
+    @dishes = Dish.all
+    @apps = Dish.where(category: 'Appetizer')
+    @soups = Dish.where(category: 'Soup')
+    @salads = Dish.where(category: 'Salad')
+    @sandwiches = Dish.where(category: 'Sandwich')
+    @pastas = Dish.where(category: 'Pasta')
+    @entrees = Dish.where(category: 'Entree')
+    @desserts = Dish.where(category: 'Dessert')
+
   end
 
   def manage
+    @dishes = Dish.all
   end
 
   def new
+
   end
 
   def edit
@@ -15,6 +26,16 @@ class DishesController < ApplicationController
   end
 
   def create
+    @dish = Dish.new(dish_params)
+    @dish.update(status: "active")
+    
+    if @dish.save
+      flash[:notice] = "Your dish has been added"
+      redirect_to manage_path
+    else
+      flash[:alert] = "There was a problem with your dish"
+      redirect_to "new_dish_path"
+    end
   end
 
   def update
@@ -24,5 +45,9 @@ class DishesController < ApplicationController
   end
 
   private
+
+  def dish_params
+    params.require(:dish).permit(:category, :title, :description, :price)
+  end
 
 end
